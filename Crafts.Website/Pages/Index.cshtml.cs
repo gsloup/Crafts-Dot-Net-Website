59d6094a2311b0fc878e945c80b77d2b6794f83a
Garrett Sloup <garrettsloup@gmail.com>
2020-11-09 07:55:37 -0600

    Add project files.

---
+++ Crafts.Website/Pages/Index.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Crafts.Website.Models;
+using Crafts.Website.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Crafts.Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        
+        // uses a private setter so users can't mess with data
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService) // Adds the product service
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+        // When someone "gets" this page, do the following...
+        public void OnGet()
+        {
+            // Get the products from productService and sets it to a variable to be used in HTML
+            Products = ProductService.GetProducts();
+        }
+    }
+}
---
+++ Crafts.Website/Services/JsonFileProductService.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using Crafts.Website.Models;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Crafts.Website.Services
+{
+    public class JsonFileProductService
+    {
+        // This is the constructor
+        public JsonFileProductService(IWebHostEnvironment webHostEnvironment)
+        {
+            WebHostEnvironment = webHostEnvironment;
+        }
+
+        public IWebHostEnvironment WebHostEnvironment { get; }
+
+        // Used to route to the specific data file in the webHostEnvironment
+        private string JsonFileName
+        {
+            get { return Path.Combine(WebHostEnvironment.WebRootPath, "data", "products.json"); }
+        }
+        
+        // Deserializing the JSON data into something that can be actually used
+        public IEnumerable<Product> GetProducts()
+        {
+            using (var jsonFileReader = File.OpenText(JsonFileName))
+            {
+                // Returns a list of Products
+                return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
+                    new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+            }
+        }
+
+        public void AddRating(string productId, int rating)
+        {
+            var products = GetProducts();
+
+            // Retrieving the product by its productId
+            var query = products.First(x => x.Id == productId);
+
+            // If you don't find a rating, create a new list
+            if(query.Ratings == null)
+            {
+                query.Ratings = new int[] { rating };
+            }
+            // If you find some ratings, convert them to a list
+            else
+            {
+                var ratings = query.Ratings.ToList();
+                ratings.Add(rating);
+                query.Ratings = ratings.ToArray();
+            }
+
+            using (var outputStream = File.OpenWrite(JsonFileName))
+            {
+                JsonSerializer.Serialize<IEnumerable<Product>>(
+                    new Utf8JsonWriter(outputStream, new JsonWriterOptions
+                    {
+                        SkipValidation = true,
+                        Indented = true
+                    }),
+                    products
+                ); 
+            }
+        }
+    }
+}
